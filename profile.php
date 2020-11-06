ae8c30762fd3a844a0d2d996c7b1a96730afdd4c
danzzzak <siriuz2000@mail.ru>
2020-11-06 12:44:42 +0500

    Страница доб-ния студента

--- profile.php
+++ profile.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <?php
-    $website_title = 'index';
+    $website_title = 'PROFILE';
     require 'blocks/head.php'
     ?>
     
@@ -11,9 +11,67 @@
 
 <?php require 'blocks/header.php'?>
 
+<main class="container">
+    <h3>ADD INFU</h3>
+    <form action="" method="post">
+        <label for="name">name</label>
+        <input type="text" name="name" id="name" class="form-control">
 
-    <?php
-    echo 'PROFILE'
-    ?>
+        <label for="lastname">lastname</label>
+        <input type="text" name="lastname" id="lastname" class="form-control">
+
+        <label for="ngroup">ngroup</label>
+        <input type="text" name="ngroup" id="ngroup" class="form-control">
+
+        <label for="email">email</label>
+        <input type="text" name="email" id="email" class="form-control">
+
+        <label for="points">points</label>
+        <input type="text" name="points" id="points" class="form-control">
+
+        <label for="year">year</label>
+        <input type="text" name="year" id="year" class="form-control">
+        
+        <label for="city">city</label>
+        <input type="text" name="city" id="city" class="form-control">
+
+        <button type="button" id="accept" class="btn btn-success mt-5">
+            Voity
+        </button>
+    </form>
+</main>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+    $('#accept').click(function () {
+        const name = $('#name').val();
+        const lastname = $('#lastname').val();
+        const ngroup = $('#ngroup').val();
+        const email = $('#email').val();
+        const points = $('#points').val();
+        const year = $('#year').val();
+        const city = $('#city').val();
+        console.log(name, lastname, email, year);
+
+        $.ajax( {
+            url: 'add_profile.php',
+            type: 'POST',
+            cache: false,
+            data: {'name': name, 'lastname': lastname, 'ngroup': ngroup, 'email': email, 'points': points, 'year': year, 'city': city},
+            dataType: 'html',
+            success: function() {
+                if (data == 'Ready') {
+                    console.log('all okey');
+                }
+                else  {console.log('OSHIBKA')}
+            }
+        })
+    })
+
+
+
+</script>
+
 </body>
 </html>
