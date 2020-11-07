f76786a0f1cc492e9816a0fa0fce1e04f298504a
danzzzak <siriuz2000@mail.ru>
2020-11-07 08:35:45 +0500

    Ввывод таблицы из БД

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
 
 <?php require 'blocks/header.php'?>
     <div class="table">
+        <?php
+        require_once 'mysql.php';
+
+        $sql = 'SELECT * FROM `student` ORDER BY `id` DESC';
+        $query = $pdo->query($sql);
+        while ($row = $query->fetch(PDO::FETCH_OBJ) ) {
+            $nametable = $row->name;
+        }
+        ?>
         <div class="table__header">
             <div class="row">
                 <div class="list col-md-6">Stisok</div>
@@ -26,15 +35,19 @@
                 <div class="points col-md-3">points</div>
             </div>
         </div>
+
+        <div class="table__result">
+            <div class="row mt-5">
+                <div class="name col-md-3" ><?php require 'table/table-name.php' ?>     </div>
+                <div class="last-name col-md-3"> <?php require 'table/table-lastname.php' ?></div>
+                <div class="ngroup col-md-3"> <?php require 'table/table-ngroup.php' ?>   </div>
+                <div class="points col-md-3"> <?php require 'table/table-points.php' ?>   </div>
+            </div>
+        </div>
     
         <div class="table__pages">
     
         </div>
     </div>
-
-
-    <?php
-    echo 'hello'
-    ?>
 </body>
 </html>
---
+++ table/table-name.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'mysql.php';
+
+$sql = 'SELECT * FROM `student` ORDER BY `id` DESC';
+$query = $pdo->query($sql);
+while ($row = $query->fetch(PDO::FETCH_OBJ) ) {
+    echo "<div>$row->name</div>";
+}
